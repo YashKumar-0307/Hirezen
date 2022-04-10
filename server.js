09567cb21a6b21289b47afcf3f0597238df64f2b
Yash Kumar <ykumar1_be19@thapar.edu>
2022-04-10 11:12:04 +0530

    Database setup

--- server.js
+++ server.js
@@ -4,10 +4,30 @@ const cors = require('cors');
 const app = express();
 const mongoose = require('mongoose');
 const PORT=process.env.PORT || 3001;
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(cors());
 
+async function dbconnect()
+{
+    try
+    {
+        const uri ="mongodb+srv://hirezen:gpsygpsy@cluster0.ktnki.mongodb.net/hirezen";
+        mongoose.connect(uri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true
+        });
+        const connection = mongoose.connection;
+        connection.once('open', () => {  console.log("MongoDB database connection established successfully");    })
+    }
+    catch(error)
+    {
+        console.log("MongoDB connection failed");
+    }
+}
+dbconnect();
 
 app.get('/',(req,res)=>{
     res.send('Server Running');
