fa745e2870b5e34f2ca37c37d6ff90335b6df298
shubham21699 <shubham21699@gmail.com>
2022-05-01 10:50:23 +0530

    Service/Job Post form page added.

--- client/src/pages/Post.jsx
+++ client/src/pages/Post.jsx
@@ -1,14 +1,180 @@
-import React from 'react';
+import React, { useState } from "react";
 import DefaultLayout from "../components/DefaultLayout";
+import { Row, Col, Form, Tabs, Input, Button, Select } from "antd";
+import { useDispatch } from "react-redux";
+import { updateUser } from "../redux/actions/userActions";
+
+const { TabPane } = Tabs;
+const { TextArea } = Input;
+const { Option } = Select;
 
 function Post() {
+  const [jobInfo, setJobInfo] = useState({});
+  const [activeTab, setActiveTab] = useState("0");
+
+  function onFirstFormFinish(values) {
+    setJobInfo(values);
+    setActiveTab("1");
+  }
+
+  function onFinalFormFinish(values) {
+    const finalObj = { ...jobInfo, ...values };
+    console.log(finalObj);
+  }
+
   return (
     <div>
       <DefaultLayout>
-        <h1>Post</h1>
+        <Tabs defaultActiveKey="0" activeKey={activeTab}>
+          <TabPane tab="Job Info" key="0">
+            <Form layout="vertical" onFinish={onFirstFormFinish}>
+              <Row gutter={16}>
+                <Col lg={8} sm={24}>
+                  <Form.Item
+                    name="title"
+                    rules={[{ required: true }]}
+                    label="Title"
+                  >
+                    <Input />
+                  </Form.Item>
+                </Col>
+                <Col lg={8} sm={24}>
+                  <Form.Item
+                    name="department"
+                    rules={[{ required: true }]}
+                    label="Department"
+                  >
+                    <Input />
+                  </Form.Item>
+                </Col>
+                <Col lg={8} sm={24}>
+                  <Form.Item
+                    name="experience"
+                    rules={[{ required: true }]}
+                    label="Experience"
+                  >
+                    <Input />
+                  </Form.Item>
+                </Col>
+                <Col lg={8} sm={24}>
+                  <Form.Item
+                    name="salaryFrom"
+                    rules={[{ required: true }]}
+                    label="Salary From"
+                  >
+                    <Input type="number" />
+                  </Form.Item>
+                </Col>
+                <Col lg={8} sm={24}>
+                  <Form.Item
+                    name="salaryTo"
+                    rules={[{ required: true }]}
+                    label="Salary To"
+                  >
+                    <Input type="number" />
+                  </Form.Item>
+                </Col>
+              </Row>
+              <Row gutter={16}>
+                <Col lg={8} sm={24}>
+                  <Form.Item
+                    name="skills"
+                    rules={[{ required: true }]}
+                    label="Skills"
+                  >
+                    <Input />
+                  </Form.Item>
+                </Col>
+                <Col lg={8} sm={24}>
+                  <Form.Item
+                    name="minimumQualification"
+                    rules={[{ required: true }]}
+                    label="Minimum Qualification"
+                  >
+                    <Select>
+                      <Option value="Degree">Degree</Option>
+                      <Option value="12th">12th</Option>
+                      <Option value="10th">10th</Option>
+                    </Select>
+                  </Form.Item>
+                </Col>
+                <Col lg={24} sm={24}>
+                  <Form.Item
+                    name="smallDescription"
+                    rules={[{ required: true }]}
+                    label="Small Description"
+                  >
+                    <TextArea rows={3} />
+                  </Form.Item>
+                </Col>
+                <Col lg={24} sm={24}>
+                  <Form.Item
+                    name="fullDescription"
+                    rules={[{ required: true }]}
+                    label="Full Description"
+                  >
+                    <TextArea rows={7} />
+                  </Form.Item>
+                </Col>
+              </Row>
+              <Button htmlType="submit">Next</Button>
+            </Form>
+          </TabPane>
+          <TabPane tab="Company Info" key="1">
+            <Form layout="vertical" onFinish={onFinalFormFinish}>
+              <Row gutter={16}>
+                <Col lg={8} sm={24}>
+                  <Form.Item
+                    name="company"
+                    rules={[{ required: true }]}
+                    label="Company Name"
+                  >
+                    <Input />
+                  </Form.Item>
+                </Col>
+                <Col lg={8} sm={24}>
+                  <Form.Item
+                    name="email"
+                    rules={[{ required: true }]}
+                    label="Company Email"
+                  >
+                    <Input />
+                  </Form.Item>
+                </Col>
+                <Col lg={8} sm={24}>
+                  <Form.Item
+                    name="phoneNumber"
+                    rules={[{ required: true }]}
+                    label="Phone Number"
+                  >
+                    <Input />
+                  </Form.Item>
+                </Col>
+
+                <Col lg={24} sm={24}>
+                  <Form.Item
+                    name="companyDescription"
+                    rules={[{ required: true }]}
+                    label="Company Description"
+                  >
+                    <TextArea rows={5} />
+                  </Form.Item>
+                </Col>
+              </Row>
+              <Button
+                onClick={() => {
+                  setActiveTab("0");
+                }}
+              >
+                Previous
+              </Button>
+              <Button htmlType="submit">Post Job</Button>
+            </Form>
+          </TabPane>
+        </Tabs>
       </DefaultLayout>
     </div>
   );
 }
 
-export default Post
+export default Post;
