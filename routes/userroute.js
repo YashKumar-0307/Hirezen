c3e762c9d8dc3a3614dd63a0c2e85689fad8a955
Shubham Goel <ykumar1_be19@thapar.edu>
2022-05-10 16:57:31 +0530

    Added user update functionality

--- routes/userroute.js
+++ routes/userroute.js
@@ -41,4 +41,13 @@ router.post("/update", async (req, res) => {
   }
 });
 
+router.get("/getallusers", async (req, res) => {
+  try {
+    const users = await User.find();
+    res.send(users);
+  } catch (error) {
+    return res.status(400).json({ error });
+  }
+});
+
 module.exports = router;
