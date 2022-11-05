969a3b595c544b2f1856813de7a245224abd43a3
Shubham Goel <ykumar1_be19@thapar.edu>
2022-11-05 14:58:48 +0530

    Adding Bcrypt

--- routes/userroute.js
+++ routes/userroute.js
@@ -1,11 +1,20 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 const User = require("../models/userSchema.js");
 
 router.post("/register", async (req, res) => {
   try {
-    const newUser = new User(req.body);
-    const user = await newUser.save();
+    // console.log(req.body);
+    await bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+      if(err)
+      {
+        return res.status(400).json({ err });
+      }
+      const newUser = new User({username:req.body.username,password: hash});
+      const user = newUser.save();
+    });
     res.send("User Created Successfully");
   } catch (error) {
     return res.status(400).json({ error });
@@ -16,11 +25,19 @@ router.post("/login", async (req, res) => {
   try {
     const user = await User.findOne({
       username: req.body.username,
-      password: req.body.password,
     });
-    //console.log(user);
     if (user) {
-      res.send(user);
+      // console.log(user.password);
+      const match = await bcrypt.compare(req.body.password, user.password);
+      // console.log(match);
+      if(match)
+      {
+        res.send(user);
+      }
+      else
+      {
+        return res.status(400).json({ message: "Invalid Credentials" });
+      }
     } else {
       return res.status(400).json({ message: "Invalid Credentials" });
     }
